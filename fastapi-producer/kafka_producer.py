d49d6231a9c68452a111b5e27a6f772d0ca188f4
Siddharth Mishra <geeksid@gmail.com>
2025-10-05 16:52:40 +0530

    minor bugfixes

--- fastapi-producer/kafka_producer.py
+++ fastapi-producer/kafka_producer.py
@@ -13,16 +13,16 @@ def serializer(message):
     return json.dumps(message).encode() #converts a dict to a JSON string and encode; default encoding is UTF-8
 
 producer = KafkaProducer(
-    api_version = (3,9), #latest stable version, has all feautures
-    bootstrap_servers = KAFKA_BROKER_URL,
-    serializer = serializer,
-    client_id = PRODUCER_CLIENT_ID
+    api_version = (3, 9),  # latest stable version, has all features
+    bootstrap_servers=KAFKA_BROKER_URL,
+    value_serializer=serializer,  # use value_serializer for message encoding
+    client_id=PRODUCER_CLIENT_ID
 )
 
-def produce_kafka_message(message: ProduceMessage):
+def produce_kafka_message(message: str):
     try:
-        producer.send(KAFKA_TOPIC, json.dumps({'message':message}))
-        producer.flush() #ensures all messages are sent
+        producer.send(KAFKA_TOPIC, {'message': message})
+        producer.flush()  # ensures all messages are sent
     except Exception as e:
         print(e)
         raise HTTPException(status_code=500, detail='Failed to produce message')
